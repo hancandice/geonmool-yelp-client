7fd6f5c645aaa2a2e051518bbe1ede889d19aef2
Jeeyoung Han <64334381+hancandice@users.noreply.github.com>
2020-11-05 14:49:18 +0900

    DONE with RestaurantDetail page

--- src/routes/RestaurantDetailPage.jsx
+++ src/routes/RestaurantDetailPage.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { RestaurantsContext } from "../context/RestaurantsContext";
+import RestaurantFinder from "../apis/RestaurantFinder";
 
 const RestaurantDetailPage = () => {
-  return <div>RestaurantDetailPage</div>;
+  const { id } = useParams();
+  const { selectedRestaurant, setSelectedRestaurant } = useContext(
+    RestaurantsContext
+  );
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await RestaurantFinder.get(`/${id}`);
+        setSelectedRestaurant(response.data.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return <div>{selectedRestaurant && selectedRestaurant.restaurant.name}</div>;
 };
 
 export default RestaurantDetailPage;
